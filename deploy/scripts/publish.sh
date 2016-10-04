ea64e4ff0fb6715219f229e13b7632bf449e9f02
Christophe Bismuth <cbismuth@workit.fr>
2016-10-04 11:02:16 +0200

    Update publish script to update Maven artifact version on Git tag detection

---
+++ deploy/scripts/publish.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [ ! -z "$TRAVIS_TAG" ]
+then
+    echo "Git tag detected - set Maven POM version to [$TRAVIS_TAG]"
+    mvn versions:set -DnewVersion=$TRAVIS_TAG --settings $DEPLOY_DIR/maven/settings.xml
+else
+    echo "No Git tag detected - keep Maven POM version untouched"
+fi
+
+mvn clean deploy -P bundle,ossrh --settings $DEPLOY_DIR/maven/settings.xml -B -U
